24231b2491c0176bb4a6a97254da42ed04e4a31c
Paulo Henrique <contactph51@gmail.com>
2025-03-22 15:01:56 -0300

    Changes the main route from '/' to '/orcamentos' in the router, redirecting to the budget list.

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,7 +6,7 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
+      path: '/orcamentos',
       name: 'orçamentos',
       component: BudgetList,
     },
