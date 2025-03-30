96290c8347381002679e00143a43f4f99764a4ed
Paulo Henrique <contactph51@gmail.com>
2025-03-30 06:04:10 -0300

    Adds the route for viewing budgets, including the PreviewBudget component to display details of a specific budget.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import BudgetList from '@/views/BudgetList.vue'
 import NewBudget from '@/views/NewBudget.vue'
+import PreviewBudget from '@/views/PreviewBudget.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: '/novo-orcamento',
       name: 'new-budget',
       component: NewBudget,
+    },
+    {
+      path: '/orcamento/:id',
+      name: 'budget',
+      component: PreviewBudget,
     }
   ],
 })
