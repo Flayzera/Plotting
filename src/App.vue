f0f806650ce9486c42551d639344d1e5ff2d24c0
Paulo Henrique <contactph51@gmail.com>
2025-04-30 03:00:09 -0300

    feat: conditionally render MenuBarComponent based on login page status

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,13 @@
 <script setup lang="ts">
 import MenuBarComponent from './components/MenubarComponent.vue'
+import { useRoute } from 'vue-router'
+import { computed } from 'vue'
+
+const route = useRoute()
+const isLoginPage = computed(() => route.path === '/login')
 </script>
 
 <template>
-  <MenuBarComponent />
+  <MenuBarComponent v-if="!isLoginPage" />
   <router-view />
 </template>
